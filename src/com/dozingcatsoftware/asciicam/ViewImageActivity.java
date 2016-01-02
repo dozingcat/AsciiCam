8d556687206f9cef9531c853aa8b9305f1675706
Brian Nenninger <bnenning@gmail.com>
2016-01-02 01:49:56 -0500

    Add dialog to choose to share image/html/text. Use Material theme if available.

--- src/com/dozingcatsoftware/asciicam/ViewImageActivity.java
+++ src/com/dozingcatsoftware/asciicam/ViewImageActivity.java
@@ -5,6 +5,8 @@ package com.dozingcatsoftware.asciicam;
 import java.io.File;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -20,10 +22,17 @@ import com.dozingcatsoftware.util.AndroidUtils;
  */
 public class ViewImageActivity extends Activity {
 
+    enum ShareFileType {
+        IMAGE,
+        HTML,
+        TEXT,
+    }
+
     public static final int DELETE_RESULT = Activity.RESULT_FIRST_USER;
 
     ImageView imageView;
     Uri imageUri;
+    ShareFileType shareFileType;
 
     public static Intent startActivityWithImageURI(Activity parent, Uri imageURI, String type) {
         Intent intent = new Intent(parent, ViewImageActivity.class);
@@ -33,8 +42,7 @@ public class ViewImageActivity extends Activity {
     }
 
     /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+    @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.imageview);
@@ -52,7 +60,7 @@ public class ViewImageActivity extends Activity {
         catch(Exception ex) {}
     }
 
-    // touch handler methods called via onClick bindings in imageview.xml
+    // Methods called via onClick bindings in imageview.xml
     public void goBack(View view) {
         this.finish();
     }
@@ -64,21 +72,71 @@ public class ViewImageActivity extends Activity {
         this.finish();
     }
 
-    public void shareImage(View view) {
-        Intent shareIntent = new Intent(Intent.ACTION_SEND);
-        shareIntent.setType(this.getIntent().getType());
-        shareIntent.putExtra(Intent.EXTRA_STREAM, imageUri);
-        shareIntent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        startActivity(Intent.createChooser(shareIntent, "Share Picture Using:"));
+    public void sharePicture(View view) {
+        shareFileType = ShareFileType.IMAGE;
+        final String[] shareTypeLabels = {
+            getString(R.string.shareImageOptionLabel),
+            getString(R.string.shareHtmlOptionLabel),
+            getString(R.string.shareTextOptionLabel),
+        };
+        final ShareFileType[] shareTypes = {
+            ShareFileType.IMAGE,
+            ShareFileType.HTML,
+            ShareFileType.TEXT,
+        };
+
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.shareDialogTitle)
+                .setSingleChoiceItems(shareTypeLabels, 0, new DialogInterface.OnClickListener() {
+                    // Called when the user selects a file type, does not close the dialog.
+                    @Override public void onClick(DialogInterface dialog, int which) {
+                        shareFileType = shareTypes[which];
+                    }
+                })
+                .setPositiveButton(R.string.shareDialogYesLabel, new DialogInterface.OnClickListener() {
+                    // Called when the user accepts the dialog.
+                    @Override public void onClick(DialogInterface dialog, int which) {
+                        switch (shareFileType) {
+                            case IMAGE:
+                                shareImage();
+                                break;
+                            case HTML:
+                                shareHtml();
+                                break;
+                            case TEXT:
+                                shareText();
+                                break;
+                        }
+                    }
+                })
+                .setNegativeButton(R.string.shareDialogNoLabel, null)
+                .show();
     }
 
-    public void shareHtml(View view) {
+    // Share methods.
+    public void shareImage() {
+        shareFile(imageUri, this.getIntent().getType(), getString(R.string.shareImageTitle));
+    }
+
+    public void shareHtml() {
         // hack: replace .png with .html in URI sent to this activity.
         String htmlPath = imageUri.getPath().substring(0, imageUri.getPath().length()-4) + ".html";
+        Uri htmlUri = Uri.fromFile(new File(htmlPath));
+        shareFile(htmlUri, "text/html", getString(R.string.shareHtmlTitle));
+    }
+
+    public void shareText() {
+        // hack: replace .png with .txt in URI sent to this activity.
+        String textPath = imageUri.getPath().substring(0, imageUri.getPath().length()-4) + ".txt";
+        Uri textUri = Uri.fromFile(new File(textPath));
+        shareFile(textUri, "text/plain", getString(R.string.shareTextTitle));
+    }
+
+    private void shareFile(Uri uri, String mimeType, String shareLabel) {
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
-        shareIntent.setType("text/html");
-        shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(htmlPath)));
+        shareIntent.setType(mimeType);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
         shareIntent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        startActivity(Intent.createChooser(shareIntent, "Share HTML Using:"));
+        startActivity(Intent.createChooser(shareIntent, shareLabel));
     }
 }
