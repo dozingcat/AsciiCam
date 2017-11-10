6c1638d33828799878e5387a6d7e8865a756ce63
Aaron M. Bond <ambond@aaronmbond.com>
2017-11-10 01:24:50 -0600

    Added ShareFileProvider for ViewImageActivity
    * Enables sharing of files in later API levels

---
+++ app/src/main/java/com/dozingcatsoftware/asciicam/ShareFileProvider.java
@@ -0,0 +1,10 @@
+package com.dozingcatsoftware.asciicam;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ *  A FileProvider to enable sharing from ViewImageActivity
+ */
+
+public class ShareFileProvider extends FileProvider {
+}
